0f8a1e3f01b52ec7f30428b6b97effba4479c247
Johannes <johannes.ronning@outlook.com>
2024-11-02 19:35:22 +0100

    updating basic rendering to upscale textures

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,51 @@
 const rl = @import("raylib");
+const std = @import("std");
+const tmx = @import("tmx.zig");
+
+const screen_width = 800;
+const screen_height = 450;
+const render_width = 400;
+const render_height = 225;
 
 fn update() void {}
-fn draw() void {
+fn draw(render_target: rl.RenderTexture2D) void {
+    const bg_color = rl.Color.init(0x30, 0x23, 0x3a, 0xff);
+
+    // drawing within this texture to ensure consistent resolution
+    // (render_width) x (render_height)
+    // we will use this texture also as the texture we pass to the GPU
+    rl.beginTextureMode(render_target);
+
+    rl.drawText("Congrats! You created your first window!", 30, 170, 18, rl.Color.light_gray);
+
+    rl.endTextureMode();
+
     rl.beginDrawing();
-    defer rl.endDrawing();
+    rl.clearBackground(bg_color);
+
+    // we draw the texture and upscale it to the 'screen resolution'
+    // NOTE: Render texture must be y-flipped due to default OpenGL coordinates (left-bottom).
+    rl.drawTexturePro(
+        render_target.texture,
+        rl.Rectangle.init(0, 0, render_width, -render_height),
+        rl.Rectangle.init(0, 0, screen_width, screen_height),
+        rl.Vector2.init(0, 0),
+        0,
+        rl.Color.white,
+    );
+    rl.endDrawing();
 }
 
 pub fn main() !void {
-    const screenWidth = 800;
-    const screenHeight = 450;
-
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    rl.initWindow(screen_width, screen_height, "raylib-zig [core] example - basic window");
     defer rl.closeWindow();
 
     rl.setTargetFPS(60);
+    const target = rl.loadRenderTexture(render_width, render_height);
     while (!rl.windowShouldClose()) {
         update();
-        draw();
-
-        rl.clearBackground(rl.Color.white);
-
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+        draw(target);
     }
+
+    rl.unloadTexture(target.texture);
 }
