829f4e3715793fa1e537d801dd93d1a9e6723e60
Johannes <johannes.ronning@outlook.com>
2024-11-14 00:56:40 +0100

    adding demo sprites and map

---
+++ assets/temp_tileset-Sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="temp_tileset-Sheet" tilewidth="16" tileheight="16" tilecount="20" columns="10">
+ <image source="temp_tileset.png" width="160" height="32"/>
+</tileset>
